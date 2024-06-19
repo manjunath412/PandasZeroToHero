89d69a013e9a549a3fc7bdb835e000519d7bcfde
manjunath412 <159904841+manjunath412@users.noreply.github.com>
2024-06-19 06:25:18 -0500

    Update setup_directories.py

--- setup_directories.py
+++ setup_directories.py
@@ -5,8 +5,8 @@ def setup_directories(output_dir):
     # Check if output dir exists else create
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    return input_dir, output_dir
+    return output_dir
 
 output_dir = '/ready-for-processing'
 
-setup_directories(input_dir, output_dir)
+setup_directories(output_dir)
