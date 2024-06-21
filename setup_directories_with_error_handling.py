7b3b7b3c094a7082ffb2ae95e76d30f997cf76c3
manjunath412 <159904841+manjunath412@users.noreply.github.com>
2024-06-20 19:20:05 -0500

    Update setup_directories_with_error_handling.py

--- setup_directories_with_error_handling.py
+++ setup_directories_with_error_handling.py
@@ -1,12 +1,12 @@
 import os
 import pandas as pd
 
-def setup_directories(input_dir, output_dir):
+def setup_directories(output_dir):
     try:
         # Check for output directory if not exist, we will create it
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        return input_dir, output_dir
+        return output_dir
     except Exception as e:
         print(f"Error setting up directories: {e}")
         raise
